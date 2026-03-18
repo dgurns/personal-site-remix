30c9e58567557b7949a5b0f06b8380b86c4def69
Dan Gurney <dan@dangurney.net>
2026-03-18 19:26:14 +0000

    add blog post

--- app/routes/blog._index.tsx
+++ app/routes/blog._index.tsx
@@ -1,87 +1,92 @@
-import { Link } from '@remix-run/react';
-import { type MetaFunction } from '@remix-run/cloudflare';
+import { Link } from "@remix-run/react";
+import { type MetaFunction } from "@remix-run/cloudflare";
 
 export const meta: MetaFunction = () => [
-	{
-		title: 'Dan Gurney - Blog',
-	},
+  {
+    title: "Dan Gurney - Blog",
+  },
 ];
 
 interface Post {
-	title: string;
-	date: string;
-	slug: string;
+  title: string;
+  date: string;
+  slug: string;
 }
 export const posts: Post[] = [
-	{
-		title: 'Alignment Is On Its Way Out',
-		date: 'January 3, 2026',
-		slug: 'alignment'
-	},
-	{
-		title: 'What Is an AI Native Product?',
-		date: 'November 30, 2025',
-		slug: 'ai-native-product',
-	},
-	{
-		title: 'The Unexpected Effect of AI Coding Tools',
-		date: 'November 7, 2025',
-		slug: 'ai-coding-tools',
-	},
-	{
-		title: 'Father Charlie Coen, 1933-2024',
-		date: 'January 27, 2025',
-		slug: 'father-charlie'
-	},
-	{
-		title: 'AI-rish Tunes',
-		date: 'December 8, 2022',
-		slug: 'airish-tunes',
-	},
-	{
-		title: 'Building a Guestbook with Workers KV',
-		date: 'October 20, 2022',
-		slug: 'building-guestbook',
-	},
-	{
-		title: 'Experimenting with AI Chatbots',
-		date: 'October 13, 2022',
-		slug: 'experimenting-with-ai-chatbots',
-	},
-	{
-		title: 'Migrating Trad Archive from NextJS to Remix',
-		date: 'August 7, 2022',
-		slug: 'migrating-trad-archive-to-remix',
-	},
-	{
-		title: 'Promo Video for "From the Bridge"',
-		date: 'June 23, 2022',
-		slug: 'promo-from-the-bridge',
-	},
-	{
-		title: 'New Compositions for ITMA',
-		date: 'June 2, 2022',
-		slug: 'new-compositions-itma',
-	},
-	{
-		title: 'How This Site Was Made',
-		date: 'May 26, 2022',
-		slug: 'making-of',
-	},
+  {
+    title: "Autonomous Coding Agents and Long-Term Context",
+    date: "March 18, 2026",
+    slug: "coding-agents-context",
+  },
+  {
+    title: "Alignment Is On Its Way Out",
+    date: "January 3, 2026",
+    slug: "alignment",
+  },
+  {
+    title: "What Is an AI Native Product?",
+    date: "November 30, 2025",
+    slug: "ai-native-product",
+  },
+  {
+    title: "The Unexpected Effect of AI Coding Tools",
+    date: "November 7, 2025",
+    slug: "ai-coding-tools",
+  },
+  {
+    title: "Father Charlie Coen, 1933-2024",
+    date: "January 27, 2025",
+    slug: "father-charlie",
+  },
+  {
+    title: "AI-rish Tunes",
+    date: "December 8, 2022",
+    slug: "airish-tunes",
+  },
+  {
+    title: "Building a Guestbook with Workers KV",
+    date: "October 20, 2022",
+    slug: "building-guestbook",
+  },
+  {
+    title: "Experimenting with AI Chatbots",
+    date: "October 13, 2022",
+    slug: "experimenting-with-ai-chatbots",
+  },
+  {
+    title: "Migrating Trad Archive from NextJS to Remix",
+    date: "August 7, 2022",
+    slug: "migrating-trad-archive-to-remix",
+  },
+  {
+    title: 'Promo Video for "From the Bridge"',
+    date: "June 23, 2022",
+    slug: "promo-from-the-bridge",
+  },
+  {
+    title: "New Compositions for ITMA",
+    date: "June 2, 2022",
+    slug: "new-compositions-itma",
+  },
+  {
+    title: "How This Site Was Made",
+    date: "May 26, 2022",
+    slug: "making-of",
+  },
 ];
 
 export default function Blog() {
-	return (
-		<div>
-			<h1 className="mb-6">Blog</h1>
-			<ul className="space-y-4">
-				{posts.map((p, index) => (
-					<li key={index}>
-						<div className="text-gray-400">{p.date}</div>
-						<Link to={`/blog/${p.slug}`}>{p.title}</Link>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className="mb-6">Blog</h1>
+      <ul className="space-y-4">
+        {posts.map((p, index) => (
+          <li key={index}>
+            <div className="text-gray-400">{p.date}</div>
+            <Link to={`/blog/${p.slug}`}>{p.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
